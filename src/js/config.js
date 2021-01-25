1b3af1b06467eb68b51cbfe3218430238edcdd19
RobaRoba <bogdano285@gmail.com>
2021-01-25 16:24:38 +0900

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'e1617fdd-5faf-476d-bbc0-f0e268dfe88d';
+export const MODAL_CLOSE_SEC = 2.5;
